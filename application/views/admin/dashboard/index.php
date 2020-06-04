97c5f47e284d73b89691c167d2e73ebbdeec158d
yaufaniadam <yaufani@gmail.com>
2020-06-04 13:04:37 +0700

    dasbor

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -5,10 +5,74 @@
 <section class="content">
   <div class="row">
     <div class="col-lg-3 col-6">      
-    <pre><?php print_r(menu_category('ai')); ?></pre>
+      <h1 class="h2 mb-4">Selamat datang</h1>
 				
     </div>
   </div>
+
+  <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner py-4">
+                <h3></h3>
+
+                <p>Akreditasi Perguruan Tinggi</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-graduation-cap"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner py-4">
+                <h3></h3>
+
+                <p>Akreditasi Internasional</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-graduation-cap"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner py-4">
+                <h3></h3>
+
+                <p>Sertifikasi Internasional</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-graduation-cap"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner py-4">
+                <h3></h3>
+
+                <p>Akreditasi Program Studi</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-graduation-cap"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        
 </section>
 
 <script>
